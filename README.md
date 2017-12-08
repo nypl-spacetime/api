f7b89c888107fb882926c47378f32912a692abca
Rafael Hernandez <rafaelhernandez@nypl.org>
2017-12-08 15:25:37 -0500

    Updated repo with serverless API code, files and dependencies

--- README.md
+++ README.md
@@ -1,18 +1,35 @@
 # NYC Space/Time Directory API
 
-NYC Space/Time Directory API
+NYC Space/Time Directory Elastic Search API powered by AWS Lambda, Serverless and Express
 
-TODO:
+## Version
+> v0.0.1
 
-  - Convert to AWS Lambda!
-  - Two endpoints:
-    1. `/search`
-    2. `/datasets/:datasetId/objects/:objectId`
+## Supported Endpoints
+|Route   	|Example   	|Description   	|
+|---	|---	|---	|
+|`/datasets/:datasetId/objects/:objectId`   	|`/datasets/addresses/objects/104981-1`   	|Query Search for all datasets with given :datasetId and :objectId   	|
+|`/search`   	|`/search?name=53 Crosby Street`   	|Query Search for all name property matches   	|
+|   	|`/search?since=1900&until=1910`   	|Query Search for a range of dates   	|
+|   	|`/search?geometry=-73.99136982858165,40.72543088866333,-73.99136982858165,40.72543088866333`   	|Query Search by bounding box of 4 geo-coordinates (longitude, latitude)   	|
 
-Searching, e.g. `/search?name=name&type=type`:
-  - by bounding box, `northWest`, etc.
-  - years
-  - type
-  - names
-  - ID
-  
+## AWS Configuration
+Prior to deployment, you must ensure your AWS_PROFILE is set as an environment variable in your Lambda AWS Console Configuration page.
+
+## Development
+
+### Install Dependencies
+```sh
+$ npm install -g serverless // Install serverless globally
+$ npm install
+```
+
+### Offline API Mode
+```sh
+$ sls offline start
+```
+
+### Deploy to AWS Lambda
+```sh
+$ sls deploy
+```
--- index.js
+++ index.js
@@ -1,113 +1,264 @@
-const express = require('express')
-const elasticsearch = require('spacetime-db-elasticsearch')
-const cors = require('cors')
-const app = express()
+const serverless = require('serverless-http');
+const AWS = require('aws-sdk');
+const express = require('express');
+const elasticsearch = require('elasticsearch');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
 
-app.use(cors())
+const ES_INDEX = process.env.ES_INDEX || 'spacetime-graph';
+const ES_HOST = process.env.ES_HOST || 'search-spacetime-xkle7kerddv3nlg54irycxefpi.us-east-1.es.amazonaws.com';
+const ES_PORT = process.env.ES_PORT || 443;
+const AWS_PROFILE = process.env.AWS_PROFILE;
 
-const port = process.env.PORT || 3001
+let opts = {
+  host: [
+    {
+      host: ES_HOST,
+      protocol: 'https',
+      port: ES_PORT
+    }
+  ],
+  log: 'trace',
+};
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
+if (AWS_PROFILE) {
+  opts.connectionClass = require('http-aws-es');
+  opts.amazonES = {
+    region: 'us-east-1',
+    credentials: new AWS.SharedIniFileCredentials({ profile: AWS_PROFILE })
+  };
+}
 
-function toFeature (object) {
+// Instantiate ElasticSearch Client
+const esClient = new elasticsearch.Client(opts);
+
+function featureModel(object, objectKey) {
   return {
     type: 'Feature',
     properties: {
-      id: object.id,
-      dataset: object.dataset,
-      name: object.name,
-      type: object.type,
-      validSince: object.validSince,
-      validUntil: object.validUntil,
-      data: object.data
+      id: object[objectKey].id,
+      dataset: object[objectKey].dataset,
+      name: object[objectKey].name,
+      type: object[objectKey].type,
+      validSince: object[objectKey].validSince,
+      validUntil: object[objectKey].validUntil,
+      data: object[objectKey].data
     },
-    geometry: object.geometry
+    geometry: object[objectKey].geometry
+  };
+}
+
+function toFeature(object, objectKey) {
+  if (Array.isArray(object) && object.length) {
+    return {
+      type: 'FeatureCollection',
+      features: object.map(elem => featureModel(elem, objectKey))
+    };
+  }
+
+  return featureModel(object, objectKey);
+}
+
+function baseQuery(pageSize) {
+  const pgSize = pageSize || 100;
+
+  return {
+    size: pgSize,
+    query: {
+      bool: {
+        must: [],
+      }
+    }
   }
 }
 
-function search (params, res) {
-  elasticsearch.search(params, (err, objects) => {
-    if (err || !objects) {
-      const message = err.message || 'Invalid data received'
-      res.status(500).send(message)
-    } else {
-      res.send({
-        type: 'FeatureCollection',
-        features: objects.map(toFeature)
-      })
+function queryElasticSearch(index, query, res, resultStyle) {
+  if (!index) {
+    res.status(400).json({
+      error: 'The Elastic Search [INDEX] is undefined'
+    });
+  }
+  if (!query || query.length === 0) {
+    res.status(400).json({
+      error: 'The Elastic Search [QUERY] object is undefined or empty'
+    });
+  }
+
+  esClient.search({ index: index, body: query }).then(resp => {
+    const totalHits = resp.hits.total || 0;
+    const data = resp.hits.hits;
+
+    if (totalHits <= 0) {
+      res.status(404).json({
+        result: 'Not Found'
+      });
     }
-  })
+
+    if (resultStyle === 'single') {
+      res.send(toFeature(data[0], '_source'));
+    } else {
+      res.send(toFeature(data, '_source'));
+    }
+  }, (err) => {
+    res.status(400).json({
+      error: err
+    });
+  });
 }
 
+// Configuration
+app.use(bodyParser.json({ strict: false }));
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+// Index Route
 app.get('/', (req, res) => {
   res.send({
     title: 'NYC Space/Time Directory API'
   })
-})
+});
 
 app.get('/datasets/:datasetId/objects/:objectId', (req, res) => {
-  const params = {
-    datasetIds: [req.params.datasetId],
-    id: req.params.objectId
+  const { datasetId, objectId } = req.params;
+  // Handle errors for missing query params
+  if (typeof datasetId !== 'string' || datasetId === '') {
+    res.status(400).json({
+      error: 'The :datasetId query parameter was undefined or empty'
+    });
+  } else if (typeof objectId !== 'string' || objectId === '') {
+    res.status(400).json({
+      error: 'The :objectId query parameter was undefined or empty'
+    });
   }
 
-  search(params, res)
-})
+  const searchQuery = {
+    query: {
+      nested: {
+        path: 'data',
+        query: {
+          nested: {
+            path: 'data.objects',
+            query: {
+              match: {
+                'data.objects.id': `${datasetId}/${objectId}`
+              }
+            }
+          }
+        }
+      }
+    }
+  };
+
+  queryElasticSearch(ES_INDEX, searchQuery, res, 'single');
+});
+
 
 app.get('/search', (req, res) => {
-  let params = {}
+  let query = baseQuery();
 
   if (req.query.name) {
-    params.name = req.query.name
+    const name = req.query.name;
+    const field = 'name.analyzed';
+
+    query.query.bool.must.push({
+      query_string: {
+        query: name,
+        fields: [field]
+      }
+    });
   }
 
-  if (req.query.dataset) {
-    params.dataset = req.query.dataset.split(',')
+  if (req.query.types) {
+    const types = req.query.types.split(',').map(type => ({
+      type: {
+        value: type
+      }
+    }));
+
+    query.query.bool.must.push({
+      bool: {
+        should: types
+      }
+    });
   }
 
-  if (req.query.type) {
-    params.type = req.query.type.split(',')
-  }
+  if (req.query.since || req.query.until) {
+    query.query.bool.filter = [];
 
-  if (req.query.before) {
-    params.before = `${req.query.before}-12-31`
-  }
+    // Add valid since
+    if (req.query.since) {
+      const since = `${req.query.since}-01-01`;
+      query.query.bool.filter.push({
+        range: {
+          validSince: {
+            gte: since
+          }
+        }
+      });
+    }
 
-  if (req.query.after) {
-    params.after = `${req.query.after}-01-01`
+    // Add valid until
+    if (req.query.until) {
+      const until = `${req.query.until}-12-31`;
+      query.query.bool.filter.push({
+        range: {
+          validUntil: {
+            lte: until
+          }
+        }
+      });
+    }
   }
 
   if (req.query.geometry) {
-    let rect = req.query.geometry.split(',').map((c) => parseFloat(c))
-    params.geometry = [
-      [
-        rect[0],
-        rect[1]
-      ],
-      [
-        rect[2],
-        rect[3]
-      ]
-    ]
+    const coordinates = req.query.geometry.split(',');
+    const geometry = [
+      [coordinates[0], coordinates[1]],
+      [coordinates[2], coordinates[3]]
+    ];
+
+    query.query.bool.must.push({
+      bool: {
+        should: [
+          {
+            geo_bounding_box: {
+              northWest: {
+                top_left: {
+                  lat: geometry[0][1],
+                  lon: geometry[0][0]
+                },
+                bottom_right: {
+                  lat: geometry[1][1],
+                  lon: geometry[1][0]
+                }
+              }
+            }
+          },
+          {
+            geo_bounding_box: {
+              southEast: {
+                top_left: {
+                  lat: geometry[0][1],
+                  lon: geometry[0][0]
+                },
+                bottom_right: {
+                  lat: geometry[1][1],
+                  lon: geometry[1][0]
+                }
+              }
+            }
+          }
+        ]
+      }
+    });
   }
 
-  if (req.query['geometry-operation']) {
-    const operation = req.query['geometry-operation']
-    validOperations = [
-      'contains',
-      'intersects'
-    ]
+  queryElasticSearch(ES_INDEX, query, res);
+});
 
-    params.geometryOperation = validOperations.includes(operation) ? operation : validOperations[0]
-  }
-
-  search(params, res)
-})
-
-app.listen(port, () => {
-  console.log('NYC Space/Time Directory API listening on port ' + port)
-})
+module.exports.handler = serverless(app);
